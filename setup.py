aab39b5cb3d8dec426625172ef501ff43b67880c
Matthew R. Scott <gldnspud@gmail.com>
2009-07-21 11:22:53 -0700

    Require sqlalchemy

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ import sys, os
 
 version = '0.1'
 
-setup(name='blib',
-      version=version,
-      description="SqlAlchemy-based helper for reading a Billings 3 database using Python",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='ElevenCraft Inc.',
-      author_email='matt@11craft.com',
-      url='http://11craft.com/',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='blib',
+    version=version,
+    description='SqlAlchemy-based helper for reading a Billings 3 database',
+    long_description="""\
+    """,
+    classifiers=[],
+    keywords='',
+    author='ElevenCraft Inc.',
+    author_email='matt@11craft.com',
+    url='http://11craft.com/',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'sqlalchemy >= 0.5.5',
+        ],
+    entry_points="""
+        """,
+    )
