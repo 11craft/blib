103ce9f743076502567742bc629c87f4b4114239
Matthew Scott <gldnspud@gmail.com>
2009-10-21 19:28:49 -0700

    Properly allocate and initialize NSDate objects to prevent memory leak warnings; add datetime_to_nsdate corollary for nsdate_to_datetime.

--- blib/convert.py
+++ blib/convert.py
@@ -1,13 +1,25 @@
 """Data conversion functions."""
 
-import datetime
+from datetime import datetime
+from time import mktime
 
 import CoreData
 
 
 def nsdate_to_datetime(decimal):
     """Convert a NSDate decimal value to a Python datetime object."""
-    nsdate = CoreData.NSDate.dateWithTimeIntervalSinceReferenceDate_(decimal)
+    nsdate = CoreData.NSDate.alloc().initWithTimeIntervalSinceReferenceDate_(
+        decimal)
     unixtime = nsdate.timeIntervalSince1970()
-    dt = datetime.datetime.fromtimestamp(unixtime)
+    dt = datetime.fromtimestamp(unixtime)
+    del nsdate
     return dt
+
+
+def datetime_to_nsdate(dt):
+    """Convert a Python datetime object to a NSDate decimal value."""
+    unixtime = mktime(dt.timetuple()) + 1e-6*dt.microsecond
+    nsdate = CoreData.NSDate.alloc().initWithTimeIntervalSince1970_(unixtime)
+    decimal = nsdate.timeIntervalSinceReferenceDate()
+    del nsdate
+    return decimal
